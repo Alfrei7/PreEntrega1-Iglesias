7cabeea2609fa1ef56af5feacbb3471d68d1900f
Alfrei7 <111286856+Alfrei7@users.noreply.github.com>
2023-09-21 20:38:48 -0300

    Update App.js
    dsa

--- vite-project/src/components/App.js
+++ vite-project/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import NavBar from './components/NavBar'; // Asegúrate de que la ruta sea correcta
+import NavBar from './components/NavBar'; 
 import ItemListContainer from './components/ItemListContainer';
 
 function App() {
